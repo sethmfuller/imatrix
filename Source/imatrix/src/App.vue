1b3b680c13123724fc827e21793e8911789ff33c
Seth Fuller <semfulle@iupui.edu>
2018-04-14 11:04:48 -0400

    Added VueMaterial library

--- Source/imatrix/src/App.vue
+++ Source/imatrix/src/App.vue
@@ -2,16 +2,47 @@
   <div id="app">
     <img src="./assets/logo.png">
     <HelloWorld/>
+    <semipolar-spinner
+          :animation-duration="2000"
+          :size="610"
+          :color="'#2c3e50'"
+     />
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld'
+import {AtomSpinner, 
+        FlowerSpinner, 
+        HollowDotsSpinner, 
+        IntersectingCirclesSpinner, 
+        OrbitSpinner, 
+        TrinityRingsSpinner, 
+        CirclesToRhombusesSpinner,
+        SelfBuildingSquareSpinner,
+        FulfillingBouncingCircleSpinner,
+        FingerprintSpinner,
+        BreedingRhombusSpinner,
+        SpringSpinner,
+        SemipolarSpinner} from 'epic-spinners'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    AtomSpinner,
+    FlowerSpinner,
+    HollowDotsSpinner,
+    IntersectingCirclesSpinner,
+    OrbitSpinner,
+    TrinityRingsSpinner,
+    CirclesToRhombusesSpinner,
+    SelfBuildingSquareSpinner,
+    FulfillingBouncingCircleSpinner,
+    FingerprintSpinner,
+    BreedingRhombusSpinner,
+    SpringSpinner,
+    SemipolarSpinner
   }
 }
 </script>
