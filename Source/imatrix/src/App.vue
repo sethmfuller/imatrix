a9719257d7cc6753f40a30da60ab7ace478280a7
Seth Fuller <semfulle@iupui.edu>
2018-04-14 11:33:11 -0400

    Played with VueMaterial components

--- Source/imatrix/src/App.vue
+++ Source/imatrix/src/App.vue
@@ -1,10 +1,33 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+    <md-tabs class="md-primary" md-alignment="right">
+      <md-tab id="tab-home" md-label="Home"></md-tab>
+      <md-tab id="tab-pages" md-label="Pages"></md-tab>
+      <md-tab id="tab-posts" md-label="Posts"></md-tab>
+      <md-tab id="tab-favorites" md-label="Favorites"></md-tab>
+    </md-tabs>
+
+    <md-content class="md-elevation-1" style="width: 10%;">Wow</md-content>
+
+
+    <md-button class="md-icon-button md-raised md-primary">
+        <md-icon>thumb_up</md-icon>
+    </md-button>
+
+    <!-- Accent Button -->
+    <md-button class="md-icon-button md-raised md-accent">
+        <md-icon>thumb_down</md-icon>
+    </md-button>
+
+    <!-- Primary button -->
+    <md-button class="md-fab md-primary">
+        <md-icon>add</md-icon>
+    </md-button>
+
+    <!-- Spinner -->
     <semipolar-spinner
           :animation-duration="2000"
-          :size="610"
+          :size="60"
           :color="'#2c3e50'"
      />
   </div>
@@ -54,6 +77,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
